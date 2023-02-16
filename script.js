37c730a2773cc7964516d4ce2cd4c3360fffcd09
JaypeeLan <jplaniran01@gmail.com>
2023-02-16 13:02:04 +0100

    Integrated APIs

--- script.js
+++ script.js
@@ -105,10 +105,11 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+// Passing  joke to voiceRSS API
+const tellMe = (joke) => {
   VoiceRSS.speech({
     key: "05652e1295764cb3ae5be8959712fdac",
-    src: "Hello, world!",
+    src: joke,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -116,6 +117,30 @@ function test() {
     f: "44khz_16bit_stereo",
     ssml: false,
   });
-}
+};
 
-test();
+// Get jokes from API
+const getJokes = async () => {
+  let jokes = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+
+    // if the joke is a two-part joke
+    if (data.setup) {
+      jokes = `${data.setup} ...  ${data.delivery}`;
+    } else {
+      jokes = data.joke;
+    }
+
+    tellMe(jokes);
+  } catch (error) {
+    alert("Error: ", error);
+  }
+};
+
+// Event listners
+button.addEventListener("click", getJokes);
